423aaf102d5d228cf4af1289ea5cf1e5baa589dd
James Honeycutt <honeycutt.james@gmail.com>
2021-03-21 20:40:54 -0400

    Saving as hash Table

--- Get-MyWinEvents.ps1
+++ Get-MyWinEvents.ps1
@@ -3,26 +3,32 @@ function Get-WinEventIDList {
 
 # Get the list of Event IDs from my CheatSheet from GitHub
 
-$RawWebContent = Invoke-WebRequest -uri "https://github.com/P0w3rChi3f/CheatSheets/blob/master/WindowsEventIDs.md"
+  $RawWebContent = Invoke-WebRequest -uri "https://github.com/P0w3rChi3f/CheatSheets/blob/master/WindowsEventIDs.md"
 
 <#
 This line of code takes parsed the webpage down to just the EventID and Definition.  This is done by taking the just grabbing the raw content from the webpage and splitting it on each new line.  Once that is done, it then just grabs each line that starts with <td> and ends with </td>.  This is the table data that holds the data we need.  Then I was able to just grab the line property and trim the <td>
 #>
-$Content = (((($RawWebContent.RawContent).Split("`n") | Select-String -Pattern '^(<td>)[\d|\w].+(</td>)$').Line).TrimStart("<td>")).TrimEnd("</td>")
+  $Content = (((($RawWebContent.RawContent).Split("`n") | Select-String -Pattern '^(<td>)[\d|\w].+(</td>)$').Line).TrimStart("<td>")).TrimEnd("</td>")
 
-foreach ($item in $Content) {
-  if ($item -match '^\d') {
-    $ID = $item
-    }
-  else {
-    $WinEventIDs.$ID = $item
+  $EventIDHastable = @{}
 
-  } 
+  foreach ($item in $Content) {
+    if ($item -match '^\d') {
+      $ID = $item
+    } 
+    else {
+      $EventIDHastable.$ID = $item
+
+    } 
   
-}# Close Foreach Outter Loop
+  }# Close Foreach Outter Loop
 
+  
+  }
 $WinEventIDs
+} #Close function
 
-} #Close Function
 
+$WinEventLogs = Get-WinEventIDList
+$WinEventLogs 
 
